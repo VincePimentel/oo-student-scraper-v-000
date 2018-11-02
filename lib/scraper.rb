b33f56a4a54dfc6e8a2195eca0886e884dc0a1ac
Vince Andrew Pimentel <contact@vincep.me>
2018-11-02 21:06:54 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -12,14 +12,14 @@ class Scraper
 
     doc.css("div.student-card").each do |student|
       students << [
-        name: student.css("div.card-text-container h4.student-name").text,
-        location: student.css("div.card-text.container p.student-location").text,
+        name: student.css("h4.student-name").text,
+        location: student.css("p.student-location").text,
         profile_url: student.css("a").attribute("href").value
       ]
     end
 
     students
-    #binding.pry
+    binding.pry
   end
 
   def self.scrape_profile_page(profile_url)
