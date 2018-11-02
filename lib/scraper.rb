fbd5d3d3deca965955304db66cea13939dec24ff
Vince Andrew Pimentel <contact@vincep.me>
2018-11-02 20:53:57 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,8 +5,18 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     doc = Nokogiri::HTML(File.read(index_url))
-    student_card = doc.css("div.student-card")
-    binding.pry
+
+    test = doc.css("div.student-card")
+
+    students = Hash.new
+
+    doc.css("div.student-card").each do |student|
+      students = {
+        name: student.css("div.card-text-container h4").text
+        location:
+      }
+    end
+    #binding.pry
   end
 
   def self.scrape_profile_page(profile_url)
