3b4c6a09491af42b644f81d6e517ab0c2bc8ec79
Vince Andrew Pimentel <contact@vincep.me>
2018-11-02 21:15:33 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -12,11 +12,11 @@ class Scraper
 
     doc.css("div.student-card").each do |student|
       student_key = student.css("h4.student-name").text.downcase.gsub(" ", "_")
-      students[student_key.to_sym] = [
+      students[student_key.to_sym] = {
         name: student.css("h4.student-name").text,
         location: student.css("p.student-location").text,
         profile_url: student.css("a").attribute("href").value
-      ]
+      }
     end
 
     students
