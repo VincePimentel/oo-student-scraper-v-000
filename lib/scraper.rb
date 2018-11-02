2b691f8c6cb095abe8410aa84e78867be59b80d6
Vince Andrew Pimentel <contact@vincep.me>
2018-11-02 21:00:23 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -11,11 +11,11 @@ class Scraper
     students = Array.new
 
     doc.css("div.student-card").each do |student|
-      students = {
+      students = [
         name: student.css("div.card-text-container h4").text,
         location: student.css("div.card-text.container p").text,
         profile_url: student.css("a").attribute("href").value
-      }
+      ]
     end
 
     students
